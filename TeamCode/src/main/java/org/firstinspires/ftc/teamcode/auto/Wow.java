6f1fe6c00c7364b36a47dab5f143567d84219c2a
Sanjay Vijay <27sanjayvijay@gmail.com>
2023-10-27 18:21:28 -0400

    fixed apriltag detection test woohoooooo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Wow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Wow.java
@@ -1,26 +1,33 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import androidx.core.app.NotificationCompat;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.library.vision.PixelDetect;
+import org.firstinspires.ftc.teamcode.library.vision.AprilTagDetect;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
 
+@TeleOp(name = "AprilTag")
 public class Wow extends LinearOpMode {
 
     static final int STREAM_WIDTH = 1280; // modify for your camera
     static final int STREAM_HEIGHT = 720; // modify for your camera
-    OpenCvCamera webcam;
+    OpenCvWebcam webcam;
 
     public void display() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         WebcamName webcamName = null;
         webcamName = hardwareMap.get(WebcamName.class, "Webcam 1"); // put your camera's name here
         webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
-        PixelDetect pipe = new PixelDetect();
+        // PixelDetect pipe = new PixelDetect();
+        AprilTagDetect pipe = new AprilTagDetect(0.1016, 822.317, 822.317f, 319.495, 242.502);
 
         webcam.setPipeline(pipe);
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
@@ -39,8 +46,8 @@ public class Wow extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        waitForStart();
         display();
+        waitForStart();
         while (!isStarted() && !isStopRequested());
         webcam.stopStreaming();
     }
